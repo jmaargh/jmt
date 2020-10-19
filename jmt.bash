dacb1aa64b0f8b06ec0ffec10868b508038f195d
John-Mark Allen <john-mark@qu-2.com>
2020-10-19 12:13:42 +0100

    Rewrite
    - Simplifies code structure
    - Fix issue with solid colour line

--- jmt.bash
+++ jmt.bash
@@ -1,126 +1,117 @@
 # Derived from https://github.com/ohmybash/oh-my-bash/blob/master/themes/agnoster/agnoster.theme.sh
 # (which is MIT licenced)
 
-_JMT_CURRENT_BG='NONE'
-_JMT_CURRENT_RBG='NONE'
-_JMT_SEGMENT_SEPARATOR='▒'
+_JMT_SEC_BEGIN_CHARACTER=' '
+_JMT_SEC_END_CHARACTER=' '
+_JMT_END_CHARACTER=' '
 _JMT_GIT_CHANGE_CHARACTER='✶'
 _JMT_GIT_STAGED_CHARACTER='+'
 _JMT_GIT_FRESH_CHARACTER='○'
 
-function _jmt_text_effect {
-  case "$1" in
-    reset)      echo 0;;
-    bold)       echo 1;;
-    underline)  echo 4;;
-  esac
-}
-
-function _jmt_fg_colour {
-  case "$1" in
-    black)      echo 30;;
-    red)        echo 31;;
-    green)      echo 32;;
-    yellow)     echo 33;;
-    blue)       echo 34;;
-    magenta)    echo 35;;
-    cyan)       echo 36;;
-    white)      echo 37;;
-    orange)     echo 38\;5\;166;;
-  esac
-}
-
-function _jmt_bg_colour {
-  case "$1" in
-    black)      echo 40;;
-    red)        echo 41;;
-    green)      echo 42;;
-    yellow)     echo 43;;
-    blue)       echo 44;;
-    magenta)    echo 45;;
-    cyan)       echo 46;;
-    white)      echo 47;;
-    orange)     echo 48\;5\;166;;
-  esac;
+function _jmt_acc {
+  _JMT_ACC="${_JMT_ACC}${1}"
 }
 
 function _jmt_ansi {
-  local seq
-  declare -a _jmt_acodes=("${!1}")
-
-  seq=""
-  for ((i = 0; i < ${#_jmt_acodes[@]}; i++)); do
-    if [[ -n $seq ]]; then
-      seq="${seq};"
-    fi
-    seq="${seq}${_jmt_acodes[$i]}"
-  done
-
-  echo -ne '\[\033['${seq}'m\]'
+  _jmt_acc "\033[${1}m"
 }
 
-function _jmt_ansi_single {
-  echo -ne '\[\033['$1'm\]'
+function _jmt_text_effect {
+  local code=0
+  case "$1" in
+    reset)      code='0';;
+    bold)       code='1';;
+    underline)  code='4';;
+  esac
+  _jmt_ansi "$code"
 }
 
-function _jmt_ansi_colourline {
-  _jmt_ansi_single $(_jmt_bg_colour blue)
-  echo -ne '\[\033[K\]'
+function _jmt_fg_colour {
+  local code=0
+  case "$1" in
+    black)      code='30';;
+    red)        code='31';;
+    green)      code='32';;
+    yellow)     code='33';;
+    blue)       code='34';;
+    magenta)    code='35';;
+    cyan)       code='36';;
+    white)      code='37';;
+    orange)     code='38\;5\;166';;
+  esac
+  _jmt_ansi "$code"
 }
 
-function _jmt_prompt_segment {
-  local bg fg
-  declare -a _jmt_codes
-
-  _jmt_codes=("${_jmt_codes[@]}" $(_jmt_text_effect reset))
-  if [[ -n $1 ]]; then
-    bg=$(_jmt_bg_colour $1)
-    _jmt_codes=("${_jmt_codes[@]}" $bg)
-  fi
-  if [[ -n $2 ]]; then
-    fg=$(_jmt_fg_colour $2)
-    _jmt_codes=("${_jmt_codes[@]}" $fg)
-  fi
-
-  if [[ $_JMT_CURRENT_BG != NONE && $1 != $_JMT_CURRENT_BG ]]; then
-    declare -a _jmt_intermediate=($(_jmt_fg_colour $_JMT_CURRENT_BG) $(_jmt_bg_colour $1))
-    PR="$PR $(_jmt_ansi _jmt_intermediate[@])$_JMT_SEGMENT_SEPARATOR"
-    PR="$PR$(_jmt_ansi _jmt_codes[@]) "
-  else
-    PR="$PR$(_jmt_ansi _jmt_codes[@]) "
-  fi
-  _JMT_CURRENT_BG=$1
-  [[ -n $3 ]] && PR="$PR$3"
+function _jmt_bg_colour {
+  local code=0
+  case "$1" in
+    default)    code='49';;
+    black)      code='40';;
+    red)        code='41';;
+    green)      code='42';;
+    yellow)     code='43';;
+    blue)       code='44';;
+    magenta)    code='45';;
+    cyan)       code='46';;
+    white)      code='47';;
+    orange)     code='48\;5\;166';;
+  esac
+  _jmt_ansi "$code"
 }
 
-function _jmt_prompt_end {
-  if [[ -n $_JMT_CURRENT_BG ]]; then
-    declare -a _jmt_codes=($(_jmt_text_effect reset) $(_jmt_fg_colour $_JMT_CURRENT_BG))
-    PR="$PR $(_jmt_ansi _jmt_codes[@])$_JMT_SEGMENT_SEPARATOR"
-  fi
-  declare -a _jmt_reset=($(_jmt_text_effect reset))
-  PR="$PR $(_jmt_ansi _jmt_reset[@])"
-  _JMT_CURRENT_BG=''
+function _jmt_colourline {
+  _jmt_bg_colour $1
+  _jmt_acc "\033[K"
+}
+
+function _jmt_section {
+  local fg=$1
+  local bg=$2
+  local content=$3
+
+  _jmt_bg_colour "$bg"
+  _jmt_fg_colour "$fg"
+
+  _jmt_acc "${_JMT_SEC_BEGIN_CHARACTER}${content}${_JMT_SEC_END_CHARACTER}"
 }
 
 function _jmt_prompt_status {
-  local symbols
-  symbols=()
-  [[ $_JMT_RETVAL -ne 0 ]] && symbols+="$(_jmt_ansi_single $(_jmt_fg_colour red))✘"
-  [[ $UID -eq 0 ]] && symbols+="$(_jmt_ansi_single $(_jmt_fg_colour yellow))⚡"
-  [[ $(jobs -l | wc -l) -gt 0 ]] && symbols+="$(_jmt_ansi_single $(_jmt_fg_colour cyan))⚙"
-  [[ -n "$SSH_CLIENT" ]] || [[ -n "$SSH_TTY" ]] && symbols+="$(_jmt_ansi_single $(_jmt_fg_colour orange))🔗"
+  local acc_backup="${_JMT_ACC}"
+  local local_acc=""
 
-  [[ -n "$symbols" ]] && _jmt_prompt_segment black default "$symbols"
+  _JMT_ACC=""
+  if [[ $_JMT_RETVAL -ne 0 ]]; then
+    _jmt_fg_colour red
+    _jmt_acc "✘"
+  fi
+  if [[ $UID -eq 0 ]]; then
+    _jmt_fg_colour yellow
+    _jmt_acc "⚡"
+  fi
+  if [[ $(jobs -l | wc -l) -gt 0 ]]; then
+    _jmt_fg_colour cyan
+    _jmt_acc "⚙"
+  fi
+  if [[ -n "$SSH_CLIENT" ]] || [[ -n "$SSH_TTY" ]]; then
+    _jmt_fg_colour orange
+    _jmt_acc "🔗"
+  fi
+  local_acc="${_JMT_ACC}"
+  _JMT_ACC="${acc_backup}"
+
+  if [[ -n "${local_acc}" ]]; then
+    _jmt_section black default "${local_acc}"
+  fi
 }
 
 function _jmt_prompt_host {
   local user=`whoami`
-  _jmt_prompt_segment blue white "$user@\h"
+
+  _jmt_section white blue "$user@\h"
 }
 
 function _jmt_prompt_dir {
-  _jmt_prompt_segment black white '\w'
+  _jmt_section white black '\w'
 }
 
 function _jmt_git_status_dirty {
@@ -139,24 +130,24 @@ function _jmt_git_status_dirty {
 }
 
 function _jmt_prompt_git {
-  local ref dirty
+  local ref dirty background
   if $(git rev-parse --is-inside-work-tree >/dev/null 2>&1); then
-    _JMT_GIT_PROMPT_DIRTY='±'
     dirty=$(_jmt_git_status_dirty)
     ref=$(git symbolic-ref HEAD 2> /dev/null) || ref="➦ $(git show-ref --head -s --abbrev |head -n1 2> /dev/null)"
     if [[ -n $dirty ]]; then
-        _jmt_prompt_segment yellow black
+      background="yellow"
     else
-        _jmt_prompt_segment green black
+      background="green"
     fi
-    PR="$PR${ref/refs\/heads\// }$dirty"
+    _jmt_section black $background "${ref/refs\/heads\// }$dirty"
   fi
 }
 
 function _jmt_prompt_bashprompt {
-  PR="$PR $_JMT_SEGMENT_SEPARATOR$(_jmt_ansi_colourline)\n"
-  _JMT_CURRENT_BG='NONE'
-  _jmt_prompt_segment blue white '$'
+  _jmt_colourline blue
+  _jmt_fg_colour white
+  _jmt_bg_colour blue
+  _jmt_acc "\n $"
 }
 
 function _jmt_build_prompt {
@@ -165,18 +156,22 @@ function _jmt_build_prompt {
   _jmt_prompt_dir
   _jmt_prompt_git
   _jmt_prompt_bashprompt
-  _jmt_prompt_end
 }
 
 function _jmt_bash_prompt {
   _JMT_RETVAL=$?
-  # Clear the background colour
-  PR="$(_jmt_ansi_single 49)"
-  PRIGHT=''
-  _JMT_CURRENT_BG='NONE'
-  PR="$(_jmt_ansi_single $(_jmt_text_effect reset))"
+  _JMT_ACC=""
+
+  _jmt_text_effect reset
+  _jmt_bg_colour default
+  
   _jmt_build_prompt
-  PS1=$PR
+
+  _jmt_text_effect reset
+  _jmt_colourline default
+  _jmt_acc "${_JMT_END_CHARACTER}"
+
+  PS1=${_JMT_ACC}
 }
 
 PROMPT_COMMAND=_jmt_bash_prompt
